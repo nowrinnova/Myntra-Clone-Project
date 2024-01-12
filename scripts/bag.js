ad9bff9a207df2db718c3fa9e8b2e242e662f417
nowrin <nowrinnova0088@gmail.com>
2024-01-12 18:53:25 +0600

    add

--- scripts/bag.js
+++ scripts/bag.js
@@ -1,8 +1,10 @@
 let bagItemObjects;
+const CONVENIENCE_FEE=99;
 onload();
 function onload(){
     loadBagItemObjects();
     displayBagItems();
+    displayBagSummery()
 }
 function loadBagItemObjects(){
     bagItemObjects = bagItems.map(itemId =>{
@@ -14,14 +16,85 @@ function loadBagItemObjects(){
     })
 }
 function removeFromBag(itemId){
-  // bagItemObjects.pop(bagItem);
-  bagItems= bagItems.filter(bagId => { bagId !== itemId });
+  bagItems= bagItems.filter(bagId =>  bagId != itemId );
   localStorage.setItem('bagItems',JSON.stringify(bagItems));
+
 loadBagItemObjects()
 displayBagItems();
 displayBagIcon();
+displayBagSummery();
+
+}
+function displayBagSummery(){
+  let bagSummery= document.querySelector('.bag-summary');
+  let totalItem =bagItemObjects.length;
+  let totalMRP=0;
+  let discount=0;
+  let totalAmount=0;
+  bagItemObjects.forEach(item=>{
+    totalMRP += item.original_price;
+    discount += item.original_price-item.current_price;
+   totalAmount= totalMRP-discount +CONVENIENCE_FEE; 
+
+  })
+  bagSummery.innerHTML= `<div class="bag-details-container">
+    <div class="price-header">PRICE DETAILS (2 Items) </div>
+    <div class="price-item">
+      <span class="price-item-tag">Total MRP</span>
+      <span class="price-item-value">Rs ${totalMRP }</span>
+    </div>
+    <div class="price-item">
+      <span class="price-item-tag">Discount on MRP</span>
+      <span class="price-item-value priceDetail-base-discount">-Rs${discount}</span>
+    </div>
+    <div class="price-item">
+      <span class="price-item-tag">Convenience Fee</span>
+      <span class="price-item-value">Rs ${CONVENIENCE_FEE}</span>
+    </div>
+    <hr>
+    <div class="price-footer">
+      <span class="price-item-tag">Total Amount</span>
+      <span class="price-item-value">Rs ${totalAmount}</span>
+    </div>
+  </div>
+  <button class="btn-place-order">
+    <div class="css-xjhrni">PLACE ORDER</div>
+  </button>`
 }
 
+// function displayBagSummery(){
+//   let bagSummery= document.querySelector('.bag-summary');
+//   // let totalAmount=${}
+//   let innerHTML='';
+//   bagItemObjects.forEach(item => {
+
+//     innerHTML+=`<div class="bag-details-container">
+//   <div class="price-header">PRICE DETAILS (2 Items) </div>
+//   <div class="price-item">
+//     <span class="price-item-tag">Total MRP</span>
+//     <span class="price-item-value">Rs ${item.original_price}</span>
+//   </div>
+//   <div class="price-item">
+//     <span class="price-item-tag">Discount on MRP</span>
+//     <span class="price-item-value priceDetail-base-discount">-Rs${item.discount_percentage}</span>
+//   </div>
+//   <div class="price-item">
+//     <span class="price-item-tag">Convenience Fee</span>
+//     <span class="price-item-value">Rs 99</span>
+//   </div>
+//   <hr>
+//   <div class="price-footer">
+//     <span class="price-item-tag">Total Amount</span>
+//     <span class="price-item-value">Rs 1240</span>
+//   </div>
+// </div>
+// <button class="btn-place-order">
+//   <div class="css-xjhrni">PLACE ORDER</div>
+// </button>
+// `;
+// });
+//   bagSummery.innerHTML= innerHTML;
+// }
 function generateItemHTML(item){
     return `
     <div class="bag-item-container">
